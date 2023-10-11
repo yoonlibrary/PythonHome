46c0c1177765e7d5b726adad73089016f1de2068
yoonlibrary <145468660+yoonlibrary@users.noreply.github.com>
2023-10-12 00:32:11 +0900

    Delete 1011/dynamodbinfo.py

--- 1011/dynamodbinfo.py
+++
@@ -1,5 +0,0 @@
-#dynamodbinfo.py
-import boto3
-dynamodb = boto3.resource('dynamodb', region_name = 'ap-northeast-2', 
-               aws_access_key_id = 'AKIAU5VSEEQTNGWFVV66',
-               aws_secret_access_key = 
