b40746c514a81176b861a7362b0e53815ff524df
yoonlibrary <145468660+yoonlibrary@users.noreply.github.com>
2023-10-12 00:32:59 +0900

    Delete 1011 directory

--- 1011/demo5.py
+++
@@ -1,9 +0,0 @@
-#delete_item
-import boto3, dynamodbinfo
-
-table = dynamodbinfo.dynamodb.Table('Movies')
-table.delete_item(
-    Key={
-        'Code' : '20050112', 'Name' : 'Batman Begins'
-    }
-)
--- 1011/demo6.py
+++
@@ -1,5 +0,0 @@
-# delete_table
-import boto3, dynamodbinfo
-
-table = dynamodbinfo.dynamodb.Table('Movies')
-table.delete()
